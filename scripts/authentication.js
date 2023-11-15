425974dda9d04b3f41f3c18e92d9672bf159c3b6
John Park <johnbuspark@gmail.com>
2023-11-14 18:46:19 -0800

    thing

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -15,7 +15,7 @@ var uiConfig = {
           // Before this works, you must enable "Firestore" from the firebase console.
           // The Firestore rules must allow the user to write. 
           //------------------------------------------------------------------------------------------
-          var user = authResult.user;                            // get the user object from the Firebase authentication database
+          var user = authResult.user;                         // get the user object from the Firebase authentication database
           if (authResult.additionalUserInfo.isNewUser) {         //if new user
               db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
@@ -56,6 +56,9 @@ var uiConfig = {
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
   };
+
   
-  ui.start('#firebaseui-auth-container', uiConfig);
+  
+  
+
 
--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -1,10 +1,10 @@
 
 
-function querySearch(){
+function querySearch() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     let card = db.collection("hikes").doc(urlParams.get('docID'));
-    
+
     card.get("name").then(doc => {
         document.querySelector("#hikeName").innerHTML = (doc.data())["name"];
         document.querySelector(".hike-img").setAttribute(`src`, `../images/${(doc.data())["code"]}.jpg`)
@@ -12,3 +12,71 @@ function querySearch(){
     });
 }
 querySearch();
+
+function saveHikeDocumentIDAndRedirect() {
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
+
+function populateReviews() {
+    console.log("test");
+    let hikeCardTemplate = document.getElementById("reviewCardTemplate");
+    let hikeCardGroup = document.getElementById("reviewCardGroup");
+
+    let params = new URL(window.location.href); // Get the URL from the search bar
+    let hikeID = params.searchParams.get("docID");
+
+    // Double-check: is your collection called "Reviews" or "reviews"?
+    db.collection("reviews")
+        .where("hikeDocID", "==", hikeID)
+        .get()
+        .then((allReviews) => {
+            reviews = allReviews.docs;
+            console.log(reviews);
+            reviews.forEach((doc) => {
+                var title = doc.data().title;
+                var level = doc.data().level;
+                var season = doc.data().season;
+                var description = doc.data().description;
+                var flooded = doc.data().flooded;
+                var scrambled = doc.data().scrambled;
+                var time = doc.data().timestamp.toDate();
+                var rating = doc.data().rating; // Get the rating value
+                console.log(rating)
+
+                console.log(time);
+
+                let reviewCard = hikeCardTemplate.content.cloneNode(true);
+                reviewCard.querySelector(".title").innerHTML = title;
+                reviewCard.querySelector(".time").innerHTML = new Date(
+                    time
+                ).toLocaleString();
+                reviewCard.querySelector(".level").innerHTML = `Level: ${level}`;
+                reviewCard.querySelector(".season").innerHTML = `Season: ${season}`;
+                reviewCard.querySelector(".scrambled").innerHTML = `Scrambled: ${scrambled}`;
+                reviewCard.querySelector(".flooded").innerHTML = `Flooded: ${flooded}`;
+                reviewCard.querySelector(".description").innerHTML = `Description: ${description}`;
+
+                // Populate the star rating based on the rating value
+
+                // Initialize an empty string to store the star rating HTML
+                let starRating = "";
+                // This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+                for (let i = 0; i < rating; i++) {
+                    starRating += '<span class="material-icons">&#9733</span>';
+                }
+                // After the first loop, this second loop runs from i=rating to i<5.
+                for (let i = rating; i < 5; i++) {
+                    starRating += '<span class="material-icons">&#9734</span>';
+                }
+                reviewCard.querySelector(".star-rating").innerHTML = starRating;
+
+                hikeCardGroup.appendChild(reviewCard);
+            });
+        });
+}
+
+populateReviews();
+
